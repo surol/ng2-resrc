3dbec92aecf13e81f159090fd567f921403f2857
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2017-05-17 12:46:43 +0700

    Remove generated files

--- ng2-rike/disabled.directive.d.ts
+++
@@ -1,9 +0,0 @@
-import { StatusCollector } from "./status-collector";
-export declare class RikeDisabledDirective {
-    private _rikeDisabledBy;
-    private _disabledByDefault;
-    constructor(_rikeDisabledBy: StatusCollector);
-    readonly disabled: boolean;
-    rikeDisabled: boolean | undefined;
-    rikeDisabledBy: StatusCollector;
-}
--- ng2-rike/readonly.directive.d.ts
+++
@@ -1,9 +0,0 @@
-import { StatusCollector } from "./status-collector";
-export declare class RikeReadonlyDirective {
-    private _rikeReadonlyBy;
-    private _readonlyByDefault;
-    constructor(_rikeReadonlyBy: StatusCollector);
-    readonly readonly: boolean;
-    rikeReadonly: boolean | undefined;
-    rikeReadonlyBy: StatusCollector;
-}
