13b34d23b44eaa118cf02a4efac7d3c5de2b7a5a
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2017-06-05 12:19:24 +0700

    Remove ignored files

--- ng2-rike/rike.spec.d.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from "rxjs/Rx";
-import { Protocol } from "./protocol";
-export declare function setupTesting(): void;
-export declare function configureHttpTesting(): void;
-export declare function configureRikeTesting(): void;
-export declare function nextFrom<T>(op: Observable<T>): T | undefined;
-export declare function recordTo<T>(op: Observable<T>, target: T[]): T[];
-export declare function expectJsonProtocol(protocol: Protocol<any, any>): void;
